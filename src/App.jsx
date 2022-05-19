e4a6818d73d4685f137dcef6ede6df9bb33f5ade
Noam Rubin <noam@searchtalent.com>
2022-05-19 13:59:38 +0200

    added fluid length sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,19 @@
 import { useState } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import styles from "../scss/app.module.scss";
 import { CanvasContainer } from "./CanvasContainer";
 import { Diamond } from "./Diamond";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
+    <div className={styles.App}>
       <CanvasContainer fov={7} width={100}>
         <Diamond type={"shiny"} />
       </CanvasContainer>
-      <div className="logo">
-        {/* <img src="/rec_logo1.png" className="logo_img" /> */}
-        <h1 className="title">RECUNITED</h1>
+      <div className={styles.logo}>
+        <h1 className={styles.title}>RECUNITED</h1>
       </div>
-      <p className="subtitle">Talente so einzigartig wie ihr Team</p>
+      <p className={styles.subtitle}>Talente so einzigartig wie ihr Team</p>
     </div>
   );
 }
